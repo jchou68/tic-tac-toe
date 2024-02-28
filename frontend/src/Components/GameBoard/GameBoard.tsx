f6223fa449529efb61e3543eba4b6932e979df2d
jchou68 <jchou6888@gmail.com>
2024-02-28 11:17:07 -0700

    Added data to track moves and players of the game and added player icons.

--- frontend/src/Components/GameBoard/GameBoard.tsx
+++ frontend/src/Components/GameBoard/GameBoard.tsx
@@ -1,28 +1,30 @@
 import { Grid } from "@mui/material";
 import { GameTile } from "../GameTile";
-  
-const GameBoardRow = () => {
-    return (
-        <Grid container item spacing={1} alignItems="center" justifyContent="center">
-            <Grid item>
-                <GameTile/>
-            </Grid>
-            <Grid item>
-                <GameTile/>
-            </Grid>
-            <Grid item>
-                <GameTile/>
-            </Grid>
-        </Grid>
-    )
+import { type MoveType } from "../../Models";
+
+export interface GameBoardProps {
+    board: MoveType[][];
+    onSelectedMove: (row: number, col: number) => void;
 }
 
-export const GameBoard = () => {
+export const GameBoard = ({board, onSelectedMove}: GameBoardProps) => {
     return(
         <Grid container spacing={1}>
-            <GameBoardRow />
-            <GameBoardRow />
-            <GameBoardRow />
+            {
+                board.map((moveRow, i) => (
+                    <Grid container item spacing={1} alignItems="center" justifyContent="center" key={`row${i}`}>
+                        {
+                            moveRow.map((move, j) => (
+                                <Grid item key={`row${i}col${j}`}>
+                                    <GameTile row={i} col={j} {...{onSelectedMove, move}}/>
+                                </Grid>
+                            ))
+                        }
+                    </Grid>
+                )
+
+                )
+            }
         </Grid>
     );
 }
--- frontend/src/Components/GameTile/GameTile.tsx
+++ frontend/src/Components/GameTile/GameTile.tsx
@@ -1,17 +1,56 @@
+import React from 'react';
 import { Box } from "@mui/material";
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
 
-export const GameTile = () => {
+const GameTileBox = ({children, ...props} : React.HTMLAttributes<HTMLDivElement>) => {
     return (
-        <Box sx={{
-            '&:hover': {
-                backgroundColor: "#EABFFF",
-            },
-            backgroundColor: "#800080",
-            height: "150px",
-            width: "150px",
-            padding: "10px",
-        }}>
-            
+        <Box 
+            component="div"
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                '&:hover': {
+                    color: "#800080",
+                    backgroundColor: "#EABFFF",
+                },
+                color: "white",
+                backgroundColor: "#800080",
+                height: "150px",
+                width: "150px",
+                padding: "10px",
+            }}
+            {...props}
+        >
+            {children}
         </Box>
-    );
+    )
+}
+
+export interface GameTileProps {
+    row: number,
+    col: number,
+    move: string | null,
+    onSelectedMove: (row: number, col: number) => void;
+}
+
+export const GameTile = ({row, col, move, onSelectedMove}: GameTileProps) => {
+    if(move === "A") {
+        return (
+            <GameTileBox>
+                <CloseOutlinedIcon sx={{fontSize: 150}}/>
+            </GameTileBox>
+        );
+    } else if(move === "B") {
+        return (
+            <GameTileBox>
+                <CircleOutlinedIcon sx={{fontSize: 120}}/>
+            </GameTileBox>
+        );
+    } else {
+        return (
+            <GameTileBox onClick={(_) => {onSelectedMove(row, col)}}/>
+        );
+    }
 }
